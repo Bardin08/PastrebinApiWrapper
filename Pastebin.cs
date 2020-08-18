939c6765ebebbe2dc731865faab5a8a489f261b8
Vlad <asdasfb326@gmail.com>
2020-08-18 21:08:59 +0300

    Final version of PastebinAPIWrapper

--- Pastebin.cs
+++ Pastebin.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using System.Xml.Serialization;
 
 using PastebinApiWrapper.Models;
@@ -57,6 +55,7 @@ namespace PastebinApiWrapper
         {
             return await CreateNewPasteAsUserAsync(pasteInfo, "").ConfigureAwait(false);
         }
+
         /// <summary>
         /// Create new paste as user
         /// </summary>
@@ -66,9 +65,7 @@ namespace PastebinApiWrapper
         public async Task<PasteInfo> CreateNewPasteAsUserAsync(PasteInfo pasteInfo, string userApiKey)
         {
             var baseAddress = new Uri("https://pastebin.com/api/api_post.php");
-            using (var client = new HttpClient())
-            {
-                var content = new FormUrlEncodedContent(new[]
+            var content = new FormUrlEncodedContent(new[]
                 {
                     new KeyValuePair<string, string>("api_option", "paste"),
                     new KeyValuePair<string, string>("api_user_key", userApiKey),
@@ -80,52 +77,46 @@ namespace PastebinApiWrapper
                     new KeyValuePair<string, string>("api_paste_code", pasteInfo.Code)
                 });
 
-                var result = await client.PostAsync(baseAddress, content).ConfigureAwait(false);
+            var result = WebAgent.Execute(baseAddress, content).Result;
 
-                if (result.IsSuccessStatusCode)
-                {
-                    pasteInfo.Link = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
-                }
+            if (result.IsSuccessStatusCode)
+            {
+                pasteInfo.Link = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
             }
 
             return pasteInfo;
         }
+
         /// <summary>
         /// Generate user API key
         /// </summary>
         public async Task<string> CreateUserApiKey(string username, string password)
         {
-            var baseAddress = new Uri("https://pastebin.com/api/api_login.php");
-            using (var client = new HttpClient() { BaseAddress = baseAddress })
-            {
-                var content = new FormUrlEncodedContent(new[]
+            var url = new Uri("https://pastebin.com/api/api_login.php");
+            var content = new FormUrlEncodedContent(new[]
                 {
                     new KeyValuePair<string, string>("api_dev_key", DeveloperApiKey),
                     new KeyValuePair<string, string>("api_user_name", username),
                     new KeyValuePair<string, string>("api_user_password", password)
                 });
 
-                var result = await client.PostAsync(baseAddress, content).ConfigureAwait(false);
+            var result = WebAgent.Execute(url, content).Result;
 
-                if (result.IsSuccessStatusCode)
-                {
-                    UserApiKey = await result.Content.ReadAsStringAsync();
-                    return UserApiKey;
-                }
+            if (result.IsSuccessStatusCode)
+            {
+                UserApiKey = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+                return UserApiKey;
             }
             throw new HttpRequestException("Something went wrong");
         }
+
         /// <summary>
         /// Get list of user`s pastes info
         /// </summary>
-        /// <param name="resultLimit">The number of pastes that will be returned</param>
         public async Task<List<PasteInfo>> GetUserPastes(int resultsLimit = 50)
         {
-            var baseUrl = new Uri("https://pastebin.com/api/api_post.php");
-
-            using (var client = new HttpClient())
-            {
-                var content = new FormUrlEncodedContent(new[]
+            var url = new Uri("https://pastebin.com/api/api_post.php");
+            var content = new FormUrlEncodedContent(new[]
                 {
                     new KeyValuePair<string, string>("api_option", "list"),
                     new KeyValuePair<string, string>("api_user_key", UserApiKey),
@@ -133,54 +124,27 @@ namespace PastebinApiWrapper
                     new KeyValuePair<string, string>("api_results_limit", resultsLimit.ToString())
                 });
 
-                var result = await client.PostAsync(baseUrl, content).ConfigureAwait(false);
+            var result = WebAgent.Execute(url, content).Result;
 
-                if (result.IsSuccessStatusCode)
-                {
-                    var xml = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
-                    var sb = new StringBuilder();
-                    sb.Append("<pastes> ").Append(xml).Append(" </pastes>");
+            if (result.IsSuccessStatusCode)
+            {
+                var xml = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+                var sb = new StringBuilder();
+                sb.Append("<pastes> ").Append(xml).Append(" </pastes>");
 
-
-                    using (var sr = new StringReader(sb.ToString()))
-                    {
-                        var formatter = new XmlSerializer(typeof(pastes));
-
-                        var returnedPastes = formatter.Deserialize(sr) as pastes;
-                        var userPastes = new List<PasteInfo>();
-
-                        returnedPastes.paste.ToList().ForEach(p =>
-                        {
-                            userPastes.Add(new PasteInfo
-                            {
-                                Key = p.paste_key,
-                                Name = p.paste_title,
-                                Link = p.paste_url,
-                                Views = (int)p.paste_hits,
-                                Size = p.paste_size,
-                                Publication = new DateTime((long)p.paste_date),
-                                Remove = new DateTime((long)p.paste_expire_date),
-                                Language = (PasteLanguage)Enum.Parse(typeof(PasteLanguage), p.paste_format_short),
-                                Private = (PastePrivate)Enum.ToObject(typeof(PastePrivate), p.paste_private)
-                            });
-                        });
-
-                        return userPastes;
-                    }
-                }
+                return ParsePastes(sb.ToString());
             }
             throw new Exception("Something went wrong");
         }
+
         /// <summary>
         /// Get paste code in raw version
         /// </summary>
         /// <param name="pasteInfo">Paste information. Require field <seealso cref="PasteInfo.Key"/></param>
         public async Task<PasteInfo> GetRawPaste(PasteInfo pasteInfo)
         {
-            var baseAddress = new Uri("https://pastebin.com/api/api_raw.php");
-            using (var client = new HttpClient() { BaseAddress = baseAddress })
-            {
-                var content = new FormUrlEncodedContent(new[]
+            var url = new Uri("https://pastebin.com/api/api_raw.php");
+            var content = new FormUrlEncodedContent(new[]
                 {
                     new KeyValuePair<string, string>("api_option", "show_paste"),
                     new KeyValuePair<string, string>("api_user_key", UserApiKey),
@@ -188,26 +152,25 @@ namespace PastebinApiWrapper
                     new KeyValuePair<string, string>("api_paste_key", pasteInfo.Key)
                 });
 
-                var result = await client.PostAsync(baseAddress, content).ConfigureAwait(false);
+            var result = WebAgent.Execute(url, content).Result;
 
-                if (result.IsSuccessStatusCode)
-                {
-                    pasteInfo.Code = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
-                    return pasteInfo;
-                }
+            if (result.IsSuccessStatusCode)
+            {
+                pasteInfo.Code = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+                return pasteInfo;
             }
+
             throw new HttpRequestException("Something went wrong");
         }
+
         /// <summary>
         /// Delete user`s paste
         /// </summary>
-        /// <param name="pasteInfo">Paste info/ Require field <seealso cref="PasteInfo.Key"/></param>
+        /// <param name="pasteInfo">Paste info. Require field <seealso cref="PasteInfo.Key"/></param>
         public async Task<string> DeletePaste(PasteInfo pasteInfo)
         {
-            var baseAddress = new Uri("https://pastebin.com/api/api_post.php");
-            using (var client = new HttpClient() { BaseAddress = baseAddress })
-            {
-                var content = new FormUrlEncodedContent(new[]
+            var url = new Uri("https://pastebin.com/api/api_post.php");
+            var content = new FormUrlEncodedContent(new[]
                 {
                     new KeyValuePair<string, string>("api_option", "delete"),
                     new KeyValuePair<string, string>("api_user_key", UserApiKey),
@@ -215,62 +178,64 @@ namespace PastebinApiWrapper
                     new KeyValuePair<string, string>("api_paste_key", pasteInfo.Key)
                 });
 
-                var result = await client.PostAsync(baseAddress, content).ConfigureAwait(false);
+            var result = WebAgent.Execute(url, content).Result;
 
-                if (result.IsSuccessStatusCode)
-                {
-                    return await result.Content.ReadAsStringAsync().ConfigureAwait(false);
-                }
+            if (result.IsSuccessStatusCode)
+            {
+                return await result.Content.ReadAsStringAsync().ConfigureAwait(false);
             }
+
             throw new HttpRequestException("Something went wrong");
 
         }
+
+        /// <summary>
+        /// Return user`s info and settings
+        /// </summary>
         public async Task<UserInfo> GetUserInfoAndSettings()
         {
-            var baseUrl = new Uri("https://pastebin.com/api/api_post.php");
+            var url = new Uri("https://pastebin.com/api/api_post.php");
 
-            using (var client = new HttpClient())
-            {
-                var content = new FormUrlEncodedContent(new[]
+            var content = new FormUrlEncodedContent(new[]
                 {
                     new KeyValuePair<string, string>("api_option", "userdetails"),
                     new KeyValuePair<string, string>("api_user_key", UserApiKey),
                     new KeyValuePair<string, string>("api_dev_key", DeveloperApiKey),
                 });
 
-                var result = await client.PostAsync(baseUrl, content).ConfigureAwait(false);
+            var result = WebAgent.Execute(url, content).Result;
 
-                if (result.IsSuccessStatusCode)
-                {
-                    var xml = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+            if (result.IsSuccessStatusCode)
+            {
+                var xml = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
 
-                    using (var sr = new StringReader(xml))
-                    {
-                        var formatter = new XmlSerializer(typeof(user));
-
-                        var parsedUserInfo = formatter.Deserialize(sr) as user;
-
-                        return new UserInfo
-                        {
-                            Name = parsedUserInfo.user_name,
-                            FormatShort = parsedUserInfo.user_format_short,
-                            AvatarUrl = parsedUserInfo.user_avatar_url,
-                            Website = parsedUserInfo.user_website,
-                            Email = parsedUserInfo.user_email,
-                            Location = parsedUserInfo.user_location,
-                            Private = (AccountPrivate)Enum.ToObject(typeof(AccountPrivate), parsedUserInfo.user_private),
-                            AccountType = (AccountType)Enum.ToObject(typeof(AccountType), parsedUserInfo.user_account_type)
-                        };
-                    }
-                }
+                return ParseUserInfo(xml);
             }
+
             throw new Exception("Something went wrong");
 
         }
 
+
         #endregion
 
         #region Private Methods
+        /// <summary>
+        /// Parse user`s pastes
+        /// </summary>
+        /// <param name="xml">Paste bin responce</param>
+        private static List<PasteInfo> ParsePastes(string xml)
+        {
+            using (var sr = new StringReader(xml))
+            {
+                var formatter = new XmlSerializer(typeof(pastes));
+
+                var parsedPastes = formatter.Deserialize(sr) as pastes;
+
+                return ConvertParsedPastesToPasteInfo(parsedPastes);
+            }
+        }
+
         /// <summary>
         /// Convert <seealso cref="PastebinApiWrapper.Enums.PasteExpireDate"/> to string
         /// </summary>
@@ -314,6 +279,61 @@ namespace PastebinApiWrapper
             }
             throw new ArgumentException("Incorrect value", nameof(pasteExpireDate));
         }
+
+        /// <summary>
+        /// Convert <seealso cref="pastes"/> to <seealso cref="PasteInfo"/>
+        /// </summary>
+        /// <param name="parsedPastes"></param>
+        /// <returns>List of PasteInfos</returns>
+        private static List<PasteInfo> ConvertParsedPastesToPasteInfo(pastes parsedPastes)
+        {
+            var userPastes = new List<PasteInfo>();
+
+            parsedPastes.paste.ToList().ForEach(p =>
+            {
+                userPastes.Add(new PasteInfo
+                {
+                    Key = p.paste_key,
+                    Name = p.paste_title,
+                    Link = p.paste_url,
+                    Views = p.paste_hits,
+                    Size = p.paste_size,
+                    Publication = new DateTime(p.paste_date),
+                    Remove = new DateTime(p.paste_expire_date),
+                    Language = (PasteLanguage)Enum.Parse(typeof(PasteLanguage), p.paste_format_short),
+                    Private = (PastePrivate)Enum.ToObject(typeof(PastePrivate), p.paste_private)
+                });
+            });
+
+            return userPastes;
+        }
+
+        /// <summary>
+        /// Parse user`s info
+        /// </summary>
+        /// <param name="xml">Pastebin response</param>
+        /// <returns>User info</returns>
+        private static UserInfo ParseUserInfo(string xml)
+        {
+            using (var sr = new StringReader(xml))
+            {
+                var formatter = new XmlSerializer(typeof(user));
+                var parsedUserInfo = formatter.Deserialize(sr) as user;
+
+                return new UserInfo
+                {
+                    Name = parsedUserInfo.user_name,
+                    FormatShort = parsedUserInfo.user_format_short,
+                    AvatarUrl = parsedUserInfo.user_avatar_url,
+                    Website = parsedUserInfo.user_website,
+                    Email = parsedUserInfo.user_email,
+                    Location = parsedUserInfo.user_location,
+                    Private = (AccountPrivate)Enum.ToObject(typeof(AccountPrivate), parsedUserInfo.user_private),
+                    AccountType = (AccountType)Enum.ToObject(typeof(AccountType), parsedUserInfo.user_account_type)
+                };
+            }
+        }
+
         #endregion
 
         #endregion
---
+++ WebAgent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PastebinApiWrapper
+{
+    static class WebAgent
+    {
+        public static async Task<HttpResponseMessage> Execute(Uri requestedUri, FormUrlEncodedContent content)
+        {
+            using (var client = new HttpClient())
+            {
+                return await client.PostAsync(requestedUri, content).ConfigureAwait(false);
+            }
+        }
+    }
+}
